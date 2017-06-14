7fde3ab8cb157bf40e761a90fce96b92554d0180
FloppidyDingo <2014jamesjackson@gmail.com>
2017-06-13 22:08:52 -0700

    Added Event Listeners for cleaner code

---
+++ Controls/Listeners/GameKeyListener.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controls.Listeners;
+
+import java.awt.event.KeyEvent;
+
+/**
+ *
+ * @author James
+ */
+public class GameKeyListener extends GameEventListener{
+    public void keyPressed(KeyEvent e, int key){}
+    
+    public void keyReleased(KeyEvent e, int key){}
+}
---
+++ Controls/Listeners/GameMouseListener.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controls.Listeners;
+
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import motion.Vector;
+
+/**
+ *
+ * @author James
+ */
+public class GameMouseListener extends GameEventListener{
+    
+   public void mouseAnalogEvent(MouseEvent e, Vector velocity, Point postion){}
+   
+   public void mouseClick(MouseEvent e, Point position){}
+   
+   public void mousePressed(MouseEvent e, Point position){}
+   
+   public void mouseReleased(MouseEvent e, Point position){}
+}
